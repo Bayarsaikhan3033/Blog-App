36effebdb14dda77f8da3f496faa30d4f7fe4771
Bayarsaikhan <https://github.com/Bayarsaikhan3033>
2019-01-28 01:57:20 +0800

    цэс

---
+++ includes/navbar.php
@@ -0,0 +1,11 @@
+<div class="navbar">
+	<div class="logo_div">
+		<a href="index.php"><h1>LifeBlog</h1></a>
+	</div>
+	<ul>
+	  <li><a class="active" href="index.php">Нүүр хуудас</a></li>
+	  <li><a href="#news">Мэдээлэл</a></li>
+	  <li><a href="#contact">Холбоо барих</a></li>
+	  <li><a href="#about">Бидний тухай</a></li>
+	</ul>
+</div>
